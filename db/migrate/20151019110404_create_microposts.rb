3167cee83f88d0ac8301e1fa300d84e690f279ae
ken_okiji <no_sweet_without_sweat@i.softbank.jp>
2015-10-21 21:31:16 +0000

    add followings/followers

--- db/migrate/20151019110404_create_microposts.rb
+++ db/migrate/20151019110404_create_microposts.rb
@@ -1,6 +1,7 @@
 class CreateMicroposts < ActiveRecord::Migration
   def change
     create_table :microposts do |t|
+      #ユーザーと投稿の関連付け  references=>関連付け 
       t.references :user, index: true, foreign_key: true
       t.text :content
 
