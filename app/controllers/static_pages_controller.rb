930d624fd1230155fe010b57c2eb8f20f04e0037
ken_okiji <no_sweet_without_sweat@i.softbank.jp>
2015-10-27 13:14:42 +0000

    kadai3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build
-      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
-      @pagenation = Micropost.page(params[:page])
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).page(params[:page])
     end
   end
 end
